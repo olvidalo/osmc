6a61d9588d9ff523dd6836e1fd8d118cbc6ffcf8
Greg McCarthy <greg@gjmccarthy.co.uk>
2016-09-02 21:05:19 +0100

    Update build for vero2

--- package/mediacenter-next-osmc/build.sh
+++ package/mediacenter-next-osmc/build.sh
@@ -4,11 +4,9 @@
 #!/bin/bash
 
 . ../common.sh
-if [ "$1" == "rbp1" ] || [ "$1" == "rbp2" ] || [ "$1" == "vero" ] || [ "$1" == "atv" ] || [ "$1" == "vero2" ]
+if [ "$1" == "rbp1" ] || [ "$1" == "rbp2" ] || [ "$1" == "vero" ] || [ "$1" == "atv" ] || [ "$1" == "pc" ] || [ "$1" == "vero2" ]
 then
 pull_source "https://github.com/popcornmix/xbmc/archive/4d2644354751772729b83822a722fd8161bd8898.tar.gz" "$(pwd)/src"
-
-
 API_VERSION="17"
 else
 pull_source "https://github.com/xbmc/xbmc/archive/master.tar.gz" "$(pwd)/kodi"
@@ -149,7 +147,9 @@ then
 	fi
         if [ "$1" == "vero2" ]
         then
+		handle_dep "vero2-libcec-dev-osmc"
 		handle_dep "vero2-userland-dev-osmc"
+		handle_dep "vero2-libamcodec-dev-osmc"
                 handle_dep "armv7-libshairplay-dev-osmc"
                 handle_dep "armv7-librtmp-dev-osmc"
                 handle_dep "armv7-libnfs-dev-osmc"
@@ -179,6 +179,25 @@ then
 		handle_dep "libxrandr-dev"
 		handle_dep "i386-libcrossguid-dev-osmc"
 	fi
+	if [ "$1" == "pc" ]
+	then
+		handle_dep "amd64-libshairplay-dev-osmc"
+		handle_dep "amd64-librtmp-dev-osmc"
+		handle_dep "amd64-libnfs-dev-osmc"
+		handle_dep "amd64-libplatform-dev-osmc"
+		handle_dep "amd64-libdcadec-dev-osmc"
+		handle_dep "amd64-libbluray-dev-osmc"
+		handle_dep "amd64-libsqlite-dev-osmc"
+		handle_dep "libglew-dev"
+		handle_dep "libsdl1.2-dev"
+		handle_dep "libsdl-gfx1.2-dev"
+		handle_dep "libsdl-image1.2-dev"
+		handle_dep "libsdl-mixer1.2-dev"
+		handle_dep "xserver-xorg-dev"
+		handle_dep "libxrandr-dev"
+		handle_dep "x11proto-randr-dev"
+		handle_dep "amd64-libcrossguid-dev-osmc"
+	fi
 	sed '/Package/d' -i files/DEBIAN/control
 	sed '/Depends/d' -i files/DEBIAN/control
 	sed '/Package/d' -i files-debug/DEBIAN/control
@@ -193,12 +212,15 @@ then
 	pushd src/xbmc-*
 	install_patch "../../patches" "all"
 	test "$1" == atv && install_patch "../../patches" "atv"
+	test "$1" == pc && install_patch "../../patches" "pc"
 	if [ "$1" == "rbp1" ] || [ "$1" == "rbp2" ]
 	then
 		install_patch "../../patches" "rbp"
 	fi
-	if [ "$1" == "rbp1" ] || [ "$1" == "rbp2" ] || [ "$1" == "vero" ]; then install_patch "../../patches" "arm"; fi
+	if [ "$1" == "rbp1" ] || [ "$1" == "rbp2" ] || [ "$1" == "vero" ] || [ "$1" == "vero2" ]; then install_patch "../../patches" "arm"; fi
+
 	test "$1" == vero && install_patch "../../patches" "vero"
+	test "$1" == vero2 && install_patch "../../patches" "vero2"
 	./bootstrap
 	# Apple TV configuration
 	test "$1" == atv && \
@@ -223,6 +245,27 @@ then
                 --enable-libcec \
 		--disable-optimizations \
 		--enable-crystalhd
+	# PC configuration
+	test "$1" == pc && \
+	COMPFLAGS="-O3 -fomit-frame-pointer -Wl,-rpath=/usr/osmc/lib -L/usr/osmc/lib " && \
+	export CFLAGS+=${COMPFLAGS} && \
+	export CXXFLAGS+=${COMPFLAGS} && \
+	export CPPFLAGS+=${COMPFLAGS} && \
+	export LDFLAGS="" && \
+	./configure \
+		--prefix=/usr \
+		--disable-vtbdecoder \
+		--enable-vaapi \
+		--disable-vdpau \
+		--disable-pulse \
+		--enable-x11 \
+		--disable-openmax \
+		--enable-optical-drive \
+		--enable-libbluray \
+		--enable-dvdcss \
+		--disable-joystick \
+		--disable-debug \
+		--disable-optimizations
 	# Raspberry Pi Configuration
 	if [ "$1" == "rbp1" ]
 	then
@@ -289,24 +332,24 @@ then
 		--with-platform=vero \
 		--build=arm-linux
 	fi
-        if [ "$1" == "vero2" ]; then
-        LIBRARY_PATH+="/opt/vero2/lib" && \
-        COMPFLAGS="-I/opt/vero2/include -Wl,-rpath=/usr/osmc/lib -L/usr/osmc/lib " && \
-        export CFLAGS+=${COMPFLAGS} && \
-        export CXXFLAGS+=${COMPFLAGS} && \
-        export CPPFLAGS+=${COMPFLAGS} && \
-        export LDFLAGS="-L/opt/vero2/lib" && \
-        ./configure \
+		if [ "$1" == "vero2" ]; then
+		LIBRARY_PATH+="/opt/vero2/lib" && \
+		COMPFLAGS="-I/opt/vero2/include -Wl,-rpath=/usr/osmc/lib -L/usr/osmc/lib " && \
+		export CFLAGS+=${COMPFLAGS} && \
+		export CXXFLAGS+=${COMPFLAGS} && \
+		export CPPFLAGS+=${COMPFLAGS} && \
+		export LDFLAGS="-L/opt/vero2/lib" && \
+	./configure \
                 --prefix=/usr \
                 --disable-x11 \
                 --disable-openmax \
                 --disable-vdpau \
                 --disable-vaapi \
                 --enable-gles \
-                --enable-codec=aml \
+                --enable-codec=amcodec \
 		--enable-player=amplayer \
 		--enable-alsa \
-                --disable-libcec \
+                --enable-libcec \
                 --disable-debug \
                 --disable-texturepacker \
                 --enable-optical-drive \
@@ -318,7 +361,7 @@ then
                 --disable-optimizations \
                 --with-platform=vero2 \
                 --build=arm-linux
-        fi
+	    fi
 	if [ $? != 0 ]; then echo -e "Configure failed!" && umount /proc/ > /dev/null 2>&1 && exit 1; fi
 	umount /proc/ > /dev/null 2>&1
 	$BUILD
@@ -328,17 +371,17 @@ then
 	mkdir build
 	cd build
 	if [ "$1" == "rbp1" ] || [ "$1" == "rbp2" ]
- 	then
-             PLATFORM="-DCMAKE_INCLUDE_PATH=/opt/vc/include:/opt/vc/include/interface:/opt/vc/include/interface/vcos/pthreads:/opt/vc/include/interface/vmcs_host/linux -DCMAKE_LIBRARY_PATH=/opt/vc/lib"
- 	fi
- 	if [ "$1" == "vero" ]
- 	then
- 	   PLATFORM="-DCMAKE_INCLUDE_PATH=/opt/vero/lib -DCMAKE_LIBRARY_PATH=/opt/vero/include"
- 	fi
- 	if [ "$1" == "vero2" ]
- 	then
+	then
+            PLATFORM="-DCMAKE_INCLUDE_PATH=/opt/vc/include:/opt/vc/include/interface:/opt/vc/include/interface/vcos/pthreads:/opt/vc/include/interface/vmcs_host/linux -DCMAKE_LIBRARY_PATH=/opt/vc/lib"
+	fi
+	if [ "$1" == "vero" ]
+	then
+	   PLATFORM="-DCMAKE_INCLUDE_PATH=/opt/vero/lib -DCMAKE_LIBRARY_PATH=/opt/vero/include"
+	fi
+	if [ "$1" == "vero2" ]
+	then
  	   PLATFORM="-DCMAKE_INCLUDE_PATH=/opt/vero2/lib -DCMAKE_LIBRARY_PATH=/opt/vero2/include"
- 	fi
+	fi
 	cmake -DCMAKE_INSTALL_PREFIX=${out}/usr/ -DBUILD_DIR=$(pwd) ../ $PLATFORM
 	if [ $? != 0 ]; then echo "Configuring binary addons failed" && exit 1; fi
 	cd ../
@@ -373,9 +416,11 @@ then
 	strip -s ${out}/usr/lib/kodi/kodi.bin
 	COMMON_DEPENDS="niceprioritypolicy-osmc, mediacenter-send-osmc, libssh-4, libavahi-client3, python, python-imaging, python-unidecode, libsmbclient, libtiff5, libjpeg62-turbo, libsqlite3-0, libtinyxml2.6.2, libogg0, libmad0, libmicrohttpd10, libjasper1, libyajl2, libmysqlclient18, libasound2, libxml2, liblzo2-2, libxslt1.1, libpng12-0, libsamplerate0, libtag1-vanilla, libfribidi0, libgif4, libcdio13, libpcrecpp0, libfreetype6, libvorbis0a, libvorbisenc2, libass5, libcurl3, libssl1.0.0, libplist2, avahi-daemon, policykit-1, mediacenter-addon-osmc (>= 3.0.39), mediacenter-skin-osmc, diskmount-osmc (>= 1.2.9)"
 	test "$1" == atv && echo "Depends: ${COMMON_DEPENDS}, i386-libcec-osmc, i386-libnfs-osmc, i386-librtmp-osmc, i386-libshairplay-osmc, i386-libbluray-osmc, i386-libsqlite-osmc, libxrandr2, libsdl-image1.2, libglew1.10, libglu1-mesa, i386-libcrystalhd-osmc, xserver-xorg-core, xserver-xorg, xinit, xfonts-base, x11-xserver-utils, xauth, alsa-utils, xserver-xorg-video-nvidia-legacy-304xx, nvidia-xconfig, i386-libcrossguid-osmc" >> files/DEBIAN/control
+	test "$1" == pc && echo "Depends: ${COMMON_DEPENDS}, amd64-libnfs-osmc, amd64-librtmp-osmc, amd64-libshairplay-osmc, amd64-libbluray-osmc, amd64-libsqlite-osmc, libxrandr2, libsdl-image1.2, libglew1.10, libglu1-mesa, xserver-xorg-core, xserver-xorg, xinit, xfonts-base, x11-xserver-utils, xauth, alsa-utils, amd64-libcrossguid-osmc, xserver-xorg-video-intel" >> files/DEBIAN/control
 	test "$1" == rbp1 && echo "Depends: ${COMMON_DEPENDS}, rbp1-libcec-osmc (>=3.1.0-2), armv6l-libnfs-osmc, armv6l-librtmp-osmc, armv6l-libshairplay-osmc, armv6l-libbluray-osmc, armv6l-libsqlite-osmc, rbp-userland-osmc, armv6l-splash-osmc, armv6l-libcrossguid-osmc, armv6l-libdvdnav-osmc, armv6l-libdvdread-osmc, armv6l-libdvdcss-osmc" >> files/DEBIAN/control
 	test "$1" == rbp2 && echo "Depends: ${COMMON_DEPENDS}, rbp2-libcec-osmc (>=3.1.0-2), armv7-libnfs-osmc, armv7-librtmp-osmc, armv7-libshairplay-osmc, armv7-libbluray-osmc, armv7-libsqlite-osmc, rbp-userland-osmc, armv7-splash-osmc, armv7-libcrossguid-osmc, armv7-libdvdnav-osmc, armv7-libdvdread-osmc, armv7-libdvdcss-osmc" >> files/DEBIAN/control
-	test "$1" == vero2 && echo "Depends: ${COMMON_DEPENDS}, armv7-libnfs-osmc, armv7-librtmp-osmc, armv7-libshairplay-osmc, armv7-libbluray-osmc, armv7-libsqlite-osmc, vero2-userland-osmc, armv7-splash-osmc, armv7-libcrossguid-osmc, vero2-libamcodec-osmc" >> files/DEBIAN/control
+	test "$1" == vero && echo "Depends: ${COMMON_DEPENDS}, vero-libcec-osmc, armv7-libnfs-osmc, armv7-librtmp-osmc, armv7-libshairplay-osmc, armv7-libbluray-osmc, armv7-libsqlite-osmc, vero-userland-osmc, armv7-splash-osmc, armv7-libcrossguid-osmc" >> files/DEBIAN/control
+	test "$1" == vero2 && echo "Depends: ${COMMON_DEPENDS}, vero2-libcec-osmc, armv7-libnfs-osmc, armv7-librtmp-osmc, armv7-libshairplay-osmc, armv7-libbluray-osmc, armv7-libsqlite-osmc, vero2-userland-osmc, armv7-splash-osmc, armv7-libcrossguid-osmc, vero2-libamcodec-osmc" >> files/DEBIAN/control
 	cp patches/${1}-watchdog ${out}/usr/bin/mediacenter
 	cp patches/${1}-advancedsettings.xml ${out}/usr/share/kodi/system/advancedsettings.xml
 	chmod +x ${out}/usr/bin/mediacenter
