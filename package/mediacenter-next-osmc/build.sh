c5fb188f6145a87b0edd5d5ec582a2bdc47b237e
Greg McCarthy <greg@gjmccarthy.co.uk>
2016-10-03 13:10:38 +0100

    Remove sudo-install from build.sh

--- package/mediacenter-next-osmc/build.sh
+++ package/mediacenter-next-osmc/build.sh
@@ -365,7 +365,7 @@ then
 	umount /proc/ > /dev/null 2>&1
 	$BUILD
 	if [ $? != 0 ]; then echo -e "Build failed!" && exit 1; fi
-	make sudo-install DESTDIR=${out}
+	make install DESTDIR=${out}
 	pushd project/cmake/addons/
 	mkdir build
 	cd build
