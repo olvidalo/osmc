d7251835615399b37ff8392907aca09311ee2044
Greg McCarthy <greg@gjmccarthy.co.uk>
2016-10-27 21:39:13 +0100

    Bump to 234058f18b2d7fcea3eac659a36b781e527e6595

--- package/mediacenter-skin-next-osmc/build.sh
+++ package/mediacenter-skin-next-osmc/build.sh
@@ -5,5 +5,12 @@
 
 . ../common.sh
 
+REV="234058f18b2d7fcea3eac659a36b781e527e6595"
 echo -e "Building package mediacenter-skin-osmc"
+echo -e "Downloading skin"
+pull_source "https://github.com/osmc/skin.osmc/archive/${REV}.tar.gz" "$(pwd)/src"
+if [ $? != 0 ]; then echo -e "Error downloading" && exit 1; fi
+echo -e "Moving files in to place"
+mkdir -p files/usr/share/kodi/addons
+cp -ar src/skin.osmc-${REV}/ files/usr/share/kodi/addons/skin.osmc
 dpkg_build files/ mediacenter-skin-osmc.deb
