aa5f85bb880cbf0279e675c1de4da3d5b7714b6f
Greg McCarthy <greg@gjmccarthy.co.uk>
2016-12-15 09:00:14 +0000

    Bump nightly build

--- package/mediacenter-next-osmc/build.sh
+++ package/mediacenter-next-osmc/build.sh
@@ -6,7 +6,7 @@
 . ../common.sh
 if [ "$1" == "rbp1" ] || [ "$1" == "rbp2" ] || [ "$1" == "vero" ] || [ "$1" == "atv" ] || [ "$1" == "pc" ] || [ "$1" == "vero2" ]
 then
-pull_source "https://github.com/popcornmix/xbmc/archive/9953a7c7c7f020c760b63fceac737f2bcee12fac.tar.gz" "$(pwd)/src"
+pull_source "https://github.com/popcornmix/xbmc/archive/e6a6a51713ed3b09f3ed5c1d9666f8f4ec6bee45.tar.gz" "$(pwd)/src"
 API_VERSION="18"
 else
 pull_source "https://github.com/xbmc/xbmc/archive/master.tar.gz" "$(pwd)/kodi"
