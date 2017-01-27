0c00615d4cc132dbc8ba2522ac206e3a5fe42be6
Greg McCarthy <greg@gjmccarthy.co.uk>
2017-01-27 07:38:35 +0000

    Bump nightly, addin more game addons

--- package/mediacenter-next-osmc/build.sh
+++ package/mediacenter-next-osmc/build.sh
@@ -384,8 +384,8 @@ game.libretro.bsnes-mercury-balanced game.libretro.bsnes-mercury-performance gam
 game.libretro.dosbox game.libretro.fbalpha2012 game.libretro.fceumm \
 game.libretro.fmsx game.libretro.fuse \
 game.libretro.gambatte game.libretro.gw game.libretro.handy game.libretro.hatari \
-game.libretro.lutro game.libretro.meteor game.libretro.mgba game.libretro.nestopia \
-game.libretro.o2em game.libretro.pokemini game.libretro.prosystem game.libretro.quicknes \
+game.libretro.lutro game.libretro.meteor game.libretro.mgba game.libretro.nestopia game.libretro.nx \
+game.libretro.o2em game.libretro.picodrive game.libretro.pokemini game.libretro.prosystem game.libretro.quicknes \
 game.libretro.scummvm game.libretro.snes9x game.libretro.snes9x2002 game.libretro.snes9x2010 \
 game.libretro.stella game.libretro.tgbdual game.libretro.tyrquake game.libretro.vba-next \
 game.libretro.vbam game.libretro.vecx game.libretro.virtualjaguar game.libretro.yabause game.moonlight"
