0bb865fd749c8fac3e7925dbe048015f172438f0
Greg McCarthy <greg@gjmccarthy.co.uk>
2017-02-17 14:42:53 +0000

    Add game addon

--- package/mediacenter-next-osmc/build.sh
+++ package/mediacenter-next-osmc/build.sh
@@ -422,7 +422,7 @@ game.libretro.beetle-lynx game.libretro.beetle-ngp game.libretro.beetle-pce-fast
 game.libretro.beetle-pcfx game.libretro.beetle-psx game.libretro.beetle-supergrafx game.libretro.beetle-vb \
 game.libretro.beetle-wswan game.libretro.bluemsx game.libretro.bnes game.libretro.bsnes-mercury-accuracy \
 game.libretro.bsnes-mercury-balanced game.libretro.bsnes-mercury-performance game.libretro.cap32 \
-game.libretro.dosbox game.libretro.fbalpha2012 game.libretro.fceumm \
+game.libretro.dosbox game.libretro.fbalpha2012 game.libretro.fceumm game.libretro.mupen64plus \
 game.libretro.fmsx game.libretro.fuse game.libretro.prboom \
 game.libretro.gambatte game.libretro.gw game.libretro.handy game.libretro.hatari \
 game.libretro.lutro game.libretro.meteor game.libretro.mgba game.libretro.nestopia game.libretro.nx \
@@ -432,7 +432,7 @@ game.libretro.stella game.libretro.tgbdual game.libretro.tyrquake game.libretro.
 game.libretro.vbam game.libretro.vecx game.libretro.virtualjaguar game.libretro.yabause game.libretro.dinothaw"
 	# game.moonlight"
 	
-	#Broken game.libretro.reicast game.libretro.pcem game.libretro.mupen64plus	
+	#Broken game.libretro.reicast game.libretro.pcem 	
 	# game.libretro.genplus 
 
 	ADDONS_PVR="pvr.argustv pvr.demo pvr.dvblink pvr.dvbviewer pvr.filmon pvr.hdhomerun pvr.hts pvr.iptvsimple pvr.mediaportal.tvserver pvr.mythtv pvr.nextpvr pvr.njoy pvr.pctv pvr.stalker pvr.vbox pvr.vdr.vnsi pvr.vuplus pvr.wmc"
