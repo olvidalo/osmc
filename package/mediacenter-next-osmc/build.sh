7e6d1d54b6ab29b01af57a26c771357f5585e4fc
Greg McCarthy <greg@gjmccarthy.co.uk>
2017-03-04 10:19:47 +0000

    Remove broken game addon

--- package/mediacenter-next-osmc/build.sh
+++ package/mediacenter-next-osmc/build.sh
@@ -429,11 +429,11 @@ game.libretro.dosbox game.libretro.fbalpha2012 game.libretro.fceumm \
 game.libretro.fmsx game.libretro.fuse game.libretro.prboom \
 game.libretro.gambatte game.libretro.gw \
 game.libretro.lutro game.libretro.meteor game.libretro.mgba game.libretro.nestopia game.libretro.nx \
-game.libretro.o2em game.libretro.picodrive game.libretro.pokemini game.libretro.prosystem game.libretro.quicknes \
+game.libretro.o2em game.libretro.pokemini game.libretro.prosystem game.libretro.quicknes \
 game.libretro.scummvm game.libretro.snes9x game.libretro.snes9x2002 game.libretro.snes9x2010 \
 game.libretro.stella game.libretro.tgbdual game.libretro.tyrquake game.libretro.vba-next \
 game.libretro.vbam game.libretro.vecx game.libretro.virtualjaguar game.libretro.yabause game.libretro.dinothaw"
-	# game.moonlight"
+	# game.moonlight game.libretro.picodrive"
  	# game.libretro.bsnes-mercury-accuracy game.libretro.handy game.libretro.hatari	
 	#Broken game.libretro.reicast game.libretro.pcem 	
 	# game.libretro.genplus game.libretro.mupen64plus 
