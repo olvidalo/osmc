08a9c2ad8841fb1db89fbeb008a7eeee0cff6ca0
Greg McCarthy <greg@gjmccarthy.co.uk>
2017-03-09 07:59:54 +0000

    Bump nightly

--- package/mediacenter-next-osmc/build.sh
+++ package/mediacenter-next-osmc/build.sh
@@ -13,7 +13,7 @@ API_VERSION="18"
 
 #use xbmc top of tree for vero builds
 else
-pull_source "https://github.com/xbmc/xbmc/archive/f1ce03905c3798c237731c6e17e07f71fb80aae7.tar.gz" "$(pwd)/src"
+pull_source "https://github.com/xbmc/xbmc/archive/31d4e5ef7b21702c46a929e66ab5ba3a0440909e.tar.gz" "$(pwd)/src"
 API_VERSION="18"
 fi
 if [ $? != 0 ]; then echo -e "Error fetching Kodi source" && exit 1; fi
