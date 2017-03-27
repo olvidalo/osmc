26008ed9f74e50e48359f9795af8d2130de86267
Greg McCarthy <greg@gjmccarthy.co.uk>
2017-03-27 17:37:54 +0100

    Add rapidjson dependency

--- package/mediacenter-next-osmc/build.sh
+++ package/mediacenter-next-osmc/build.sh
@@ -101,6 +101,7 @@ then
 	handle_dep "libgcrypt11-dev"
 	handle_dep "git"
 	handle_dep "uuid-dev"
+	handle_dep "rapidjson-dev-osmc"
 	if [ "$1" == "rbp1" ] || [ "$1" == "rbp2" ]
 	then
 		handle_dep "rbp-userland-dev-osmc"
